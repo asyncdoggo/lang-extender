c98111d68b58bc345c2e6c686c517bcf59dbfe87
asyncdoggo <ayushdeshpande81@gmail.com>
2024-02-23 15:22:37 +0530

    add minimize button

--- content_scripts/playground.js
+++ content_scripts/playground.js
@@ -3,6 +3,7 @@
 function createPlayground() {
 
     const setSelected = (languageFromSelect, languageToSelect) => {
+        return
         const toSelectedLanguage = languageToSelect.value;
         const fromSelectedLanguage = languageFromSelect.value;
         // chrome.storage.sync.set({ 'selectedLanguage': { from: fromSelectedLanguage, to: toSelectedLanguage } });
@@ -12,45 +13,48 @@ function createPlayground() {
     let timer = null
     let timer2 = null
     const translate_url = 'http://localhost:5005/translate/'
-    // Create the floating window
-    const playgroundWindow = document.createElement('div')
-    playgroundWindow.id = 'playgroundWindow';
-    const resize_bar = document.createElement('div');
-    resize_bar.id = 'resize_bar';
-    const title = document.createElement('p');
-    title.textContent = 'Playground';
-    resize_bar.appendChild(title);
-    playgroundWindow.appendChild(resize_bar);
-    const select1 = document.createElement('select');
-    select1.id = 'language1';
-    const textArea1 = document.createElement('textarea');
-    textArea1.id = 'text1';
-    playgroundWindow.appendChild(select1);
-    playgroundWindow.appendChild(textArea1);
-    const select2 = document.createElement('select');
-    select2.id = 'language2';
-    const textArea2 = document.createElement('textarea');
-    textArea2.id = 'text2';
-    textArea2.readOnly = true;
-    playgroundWindow.appendChild(select2);
-    playgroundWindow.appendChild(textArea2);
-    const dismissButton = document.createElement('button');
-    // Append the floating window to the document body
-    detectLanguageBox = document.createElement('div');
-    detectLanguageBox.id = 'detectLanguageBox';
-    detectTextBox = document.createElement('textarea');
-    detectTextBox.id = 'detectTextBox';
-    detectTextBox.placeholder = 'Detect Language';
-    detectLanguageBox.appendChild(detectTextBox);
-    detectedLanguage = document.createElement('p');
-    detectedLanguage.id = 'detectedLanguage';
-    detectLanguageBox.appendChild(detectedLanguage);
-    playgroundWindow.appendChild(detectLanguageBox);
-    dismissButton.id = 'dismissButton';
-    dismissButton.textContent = 'Dismiss';
-    playgroundWindow.appendChild(dismissButton);
-    
+    const detect_url = 'http://localhost:5005/detect/'
+
+
+    const html = `
+    <div id="playgroundWindow">
+        <div id="resize_bar">
+            <p>Playground</p>
+            <div id="buttons">
+                <svg id="minimizeButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20">
+                    <path d="M0 192v128h512V192z"/>
+                </svg>
+
+                <svg id="dismissButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20">
+                    <path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"/>
+                </svg>
+            </div>
+        </div>
+        <select id="language1"></select>
+        <textarea id="text1"></textarea>
+        <select id="language2"></select>
+        <textarea id="text2" readonly></textarea>
+        <div id="detectLanguageBox">
+            <textarea id="detectTextBox" placeholder="Detect Language"></textarea>
+            <p id="detectedLanguage"></p>
+        </div>
+    </div>
+    `
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const playgroundWindow = doc.getElementById('playgroundWindow');
+    const resize_bar = doc.getElementById('resize_bar');
+    const select1 = doc.getElementById('language1');
+    const textArea1 = doc.getElementById('text1');
+    const select2 = doc.getElementById('language2');
+    const textArea2 = doc.getElementById('text2');
+    const dismissButton = doc.getElementById('dismissButton');
+    const minimizeButton = doc.getElementById('minimizeButton');
+    const detectTextBox = doc.getElementById('detectTextBox');
+    const detectedLanguage = doc.getElementById('detectedLanguage');
     document.body.appendChild(playgroundWindow);
+
+
 
     const styles = `
     #playgroundWindow {
@@ -71,6 +75,10 @@ function createPlayground() {
         background-color: #ccc;
         cursor: move;
         user-select: none;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
     #language1, #language2 {
         width: 100%;
@@ -87,12 +95,18 @@ function createPlayground() {
         color: black !important;
         resize: none;
     }
-    #dismissButton {
-        background-color: #f44336;
-        color: white;
-        
+
+    #buttons{
+        display: flex;
+        align-items: center;
     }
-   
+
+    #dismissButton,#minimizeButton {
+        color: white;
+        cursor: pointer;
+        padding: 2px;
+    }
+     
     #resize_bar p {
         background-color: white !important;
         color: black !important;
@@ -166,6 +180,27 @@ function createPlayground() {
         playgroundWindow.remove(); // Remove the playground window from the DOM
     });
 
+    // Minimize button functionality
+    minimizeButton.addEventListener('click', function () { 
+        if (playgroundWindow.style.height === '20px') {
+            playgroundWindow.style.height = 'auto';
+            textArea1.style.display = 'block';
+            textArea2.style.display = 'block';
+            detectTextBox.style.display = 'block';
+            detectedLanguage.style.display = 'block';
+            select1.style.display = 'block';
+            select2.style.display = 'block';
+        } else {
+            playgroundWindow.style.height = '20px';
+            textArea1.style.display = 'none';
+            textArea2.style.display = 'none';
+            detectTextBox.style.display = 'none';
+            detectedLanguage.style.display = 'none';
+            select1.style.display = 'none';
+            select2.style.display = 'none';
+        }
+    });
+
 
     // get languages from the storage
     chrome.storage.sync.get(['languages'], function (data) {
@@ -236,7 +271,7 @@ function createPlayground() {
         clearTimeout(timer2);
         timer2 = setTimeout(() => {
             const text = detectTextBox.value;
-            fetch('http://localhost:5005/detect/', {
+            fetch(detect_url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -253,7 +288,4 @@ function createPlayground() {
     })
 }
 
-
-
-// Call the function to create the playground
 createPlayground();
